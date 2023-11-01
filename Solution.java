85d60d34c2b49e0eb2cb9a6c9b36bce89a911052
mm-fiserv <147175865+mm-fiserv@users.noreply.github.com>
2023-11-01 15:37:50 -0400

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,35 +1,46 @@
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.transform.Source;
+class Node<T>
+{
+    T val;
+    Node<T> next;
 
-class Node<T> {
-  T val;
-  Node<T> next;
-
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+    public Node(T val)
+    {
+        this.val = val;
+        this.next = null;
+    }
 }
 
-class Solution {
-  public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
-  }
+class Solution
+{
 
-  public static void main() {
-    Node<String> a = new Node<>("a");
-    Node<String> b = new Node<>("b");
-    Node<String> c = new Node<>("c");
-    Node<String> d = new Node<>("d");
-    a.next = b;
-    b.next = c;
-    c.next = d;
+    public static List<String> linkedListValues(Node<String> head)
+    {
+        List<String> values = new ArrayList<>();
+        Node<String> current = head;
 
-    // a -> b -> c -> d
+        while (current != null)
+        {
+            values.add(current.val);
+            current = current.next;
+        }
+        return values;
+    }
 
-    Solution.linkedListValues(a); 
-    // -> [ "a", "b", "c", "d" ]
-  }
+    public static void main(String[] args)
+    {
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        System.out.println(Solution.linkedListValues(a));
+    }
 }
+
